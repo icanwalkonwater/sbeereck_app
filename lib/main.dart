a209afe166c2fdb4bb31fe8f1b1e60329e41e9c1
Lucas Malandrino <lucas.malandrino@gmail.com>
2021-09-27 09:09:01 +0200

    Very very WIP start

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,33 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'S\'Beer Eck',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Navigator(
+        pages: [
+          MaterialPage(
+            key: ValueKey('home'),
+            child: HomePage(),
+          )
+        ],
+      )
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Center(
+      child: Text('Hey'),
     );
   }
 }
